aa3397f1d6462525098aa17b6b6006615f1c423a
debaratibos <debaratibos@01037eac-aee2-11de-b3ee-c79121bffc40>
2009-11-22 00:22:21 +0000

    FloorPlan and FloorTile modifications

--- trunk/SourceCode/GUI/FloorPlan.cs
+++ trunk/SourceCode/GUI/FloorPlan.cs
@@ -3,9 +3,11 @@ using System.Drawing;
 using System.IO;
 using System.Diagnostics;
 using System.Collections.Generic;
+
 /* Created by Debarati-- This class can work with both .jpg and .bmp files,
  * other image types has not been tested yet.
- * we need to provide it with the full location of the image file for m_myImage property and block length for 
+ * we need to provide it with the full location of the image file for m_myImage property and also the
+ * value of m_pixelsperfoot for 
  * the array to be generated */
 
 namespace See3PO
@@ -13,41 +15,38 @@ namespace See3PO
     public class FloorPlan
     {
         private Bitmap m_myImage;
-        private double m_ppf;
-        private List<List<FloorTile>> m_floorPlanArray;
+        private double m_pixelsperfoot;
         private int m_height;
         private int m_width;
 
         public FloorPlan(Image myimage, double pixelsPerFoot)
         {
             this.m_myImage = new Bitmap(myimage);
-            this.m_ppf = pixelsPerFoot;
-            m_floorPlanArray = createArray();
+            this.m_pixelsperfoot = pixelsPerFoot;
         }
 
-        public List<List<FloorTile>> createArray()
+        public FloorTile[,] createArray()
         {
             //height and width of the original image file
             int image_width = m_myImage.Width;
             int image_height = m_myImage.Height;
 
             //height and width of the floor plan (in tiles)
-            m_height = (int)(image_height / this.m_ppf);
-            m_width = (int)(image_width / this.m_ppf);
+            m_height = (int)(image_height / this.m_pixelsperfoot);
+            m_width = (int)(image_width / this.m_pixelsperfoot);
 
-            List<List<FloorTile>> m_floorPlanArray = new List<List<FloorTile>>(m_width);
-
-
+            FloorTile[,] m_floorPlanArray = new FloorTile[m_height,m_width];
 
             // height and width of the array representation of the new array to be created
             // each cell represent the walkable or non-walkable block defined earlier
-            for (int column = 0; column < m_width; column++)
+            for (int row = 0; row < m_height; row++)
             {
-                m_floorPlanArray.Add(new List<FloorTile>());
-                for (int row = 0; row < m_height; row++)
+               for (int column = 0; column < m_width; column++)
                 {
-                    int walkVal = getWalkableValue(column, row);
-                    m_floorPlanArray[column].Add(new FloorTile(column, row, walkVal));
+                   if(getWalkableValue(column, row)== 0)
+                       m_floorPlanArray[row,column]= new FloorTile(column, row, true);
+                   else
+                       m_floorPlanArray[row, column] = new FloorTile(column, row, false);
                 }
             }
             return m_floorPlanArray;
@@ -58,16 +57,16 @@ namespace See3PO
         {
             //any color darker than gray is non walkable
             Color pixelColor;
-            
-            if (m_ppf > 1)    //if there are multiple pixels per walkable area then get the least walkable pixel color
+
+            if (m_pixelsperfoot > 1)    //if there are multiple pixels per walkable area then get the least walkable pixel color
             {
                 Color tempColor;
                 pixelColor = Color.White;
-                for (int row = 0; row < m_ppf; row++)
+                for (int row = 0; row < m_pixelsperfoot; row++)
                 {
-                    for (int column = 0; column < m_ppf; column++)
+                    for (int column = 0; column < m_pixelsperfoot; column++)
                     {
-                        tempColor = m_myImage.GetPixel((int)(X * m_ppf) + column,(int)(Y * m_ppf) + row);
+                        tempColor = m_myImage.GetPixel((int)(X * m_pixelsperfoot) + column, (int)(Y * m_pixelsperfoot) + row);
                         if (tempColor.GetBrightness() < pixelColor.GetBrightness())
                         {
                             pixelColor = tempColor;
@@ -77,17 +76,17 @@ namespace See3PO
             }
             else            // if there are multiple walkable areas per pixel, then multiply it
             {
-                pixelColor = m_myImage.GetPixel((int)(X * m_ppf), (int)(Y * m_ppf));
+                pixelColor = m_myImage.GetPixel((int)(X * m_pixelsperfoot), (int)(Y * m_pixelsperfoot));
             }
 
             Color gray = Color.Gray;
             if (pixelColor.GetBrightness() < gray.GetBrightness())
-                return 0;
-            return 99;
+                return 1;
+            return 0;
         }
 
         // prints the 2D array generated in a file.
-        public void printArray(int[,] arr, String path)
+        public void printArray(FloorTile[,] arr, String path)
         {
             // for now need to modify the location here manually
 
@@ -106,11 +105,12 @@ namespace See3PO
 
         public Bitmap toImage() {
             Bitmap b = new Bitmap(m_width, m_height);
-            for (int column = 0; column < m_width; column++) 
+            FloorTile[,] arr = createArray();
+            for (int row = 0; row < m_height; row++) 
             {
-                for (int row = 0; row < m_height; row++) 
+                for (int column = 0; column < m_width; column++) 
                 { 
-                    b.SetPixel(column, row, m_floorPlanArray[column][row].toPixel());
+                  b.SetPixel(column, row, arr[row,column].toPixel());
                 }
             }
             return b;

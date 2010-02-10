687ef58728b9190566feb029ea3f6f4ab710ff0d
frankmarinojr <frankmarinojr@01037eac-aee2-11de-b3ee-c79121bffc40>
2010-02-10 19:25:49 +0000

    renamed

---
+++ trunk/SourceCode/Host/TestHost/QGPathFinderTest.cs
@@ -0,0 +1,86 @@
+﻿using Host;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace TestHost
+{
+    
+    
+    /// <summary>
+    ///This is a test class for QGPathFinderTest and is intended
+    ///to contain all QGPathFinderTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class QGPathFinderTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for getPath
+        ///</summary>
+        [TestMethod()]
+        public void getPathTest()
+        {
+            Image myimage = new Bitmap("../../testImage.jpg"); // TODO: Initialize to an appropriate value
+            //FloorPlan fp = new FloorPlan(myimage); // TODO: Initialize to an appropriate value
+            Status status = new Status(myimage, 2.0); // TODO: Initialize to an appropriate value
+            QGPathFinder target = new QGPathFinder(status, null); // TODO: Initialize to an appropriate value
+   
+            List<FloorTile> expected = null; // TODO: Initialize to an appropriate value
+            List<FloorTile> actual;
+            actual = target.getPath();
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}

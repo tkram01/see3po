d17acd694b9a1800dfe822e1cc815f099f841d56
debaratibos <debaratibos@01037eac-aee2-11de-b3ee-c79121bffc40>
2010-02-11 19:46:17 +0000

    created separate project for FloorPlanProcessing and corresponding unit tests.

---
+++ SourceCode/FloorPlanAndTile/FloorPlanAndTile/TestFloorPlanAndTile/FloorPlanTest.cs
@@ -0,0 +1,330 @@
+﻿using FloorPlanAndTile;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Collections.Generic;
+using System;
+
+namespace TestFloorPlanAndTile
+{
+
+
+    /// <summary>
+    ///This is a test class for FloorPlanTest and is intended
+    ///to contain all FloorPlanTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class FloorPlanTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for getYTileNum
+        ///</summary>
+        [TestMethod()]
+        public void getYTileNumTest()
+        {
+            Image myimage = new Bitmap("../../testImage.jpg"); // TODO: Initialize to an appropriate value
+            FloorPlan target = new FloorPlan(myimage); // TODO: Initialize to an appropriate value
+            int expected = 89; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.getYTileNum();
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void MySerializer()
+        {
+            Image myimage = new Bitmap("../../testImage.jpg"); // TODO: Initialize to an appropriate value
+            FloorPlan target = new FloorPlan(myimage); // TODO: Initialize to an appropriate value
+            //  XmlSerializer s = new XmlSerializer(typeof (FloorPlan));
+            String testFileName = "../../testImage.xml";
+            using (Stream s = File.Open(testFileName, FileMode.Create))
+            {
+                SoapFormatter formatter = new SoapFormatter();
+                formatter.Serialize(s, target);
+            }
+
+            target = null;
+            using (Stream s = File.Open(testFileName, FileMode.Open))
+            {
+                SoapFormatter formatter = new SoapFormatter();
+                target = (FloorPlan)formatter.Deserialize(s);
+            }
+
+        }
+        /// <summary>
+        ///A test for getXTileNum
+        ///</summary>
+        [TestMethod()]
+        public void getXTileNumTest()
+        {
+            Image myimage = new Bitmap("../../testImage.jpg"); // TODO: Initialize to an appropriate value
+            FloorPlan target = new FloorPlan(myimage); // TODO: Initialize to an appropriate value
+            int expected = 95; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.getXTileNum();
+            Assert.AreEqual(expected, actual);
+
+        }
+        /* :::::::::::IMPORTANT :::::::::: 
+ * For all the tests below we need to update proper locations of the image file and also the location and name of the file to be created */
+        [TestMethod]
+        public void TestBlockSizefive() // test block size of 5
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 5);
+            image1.printArray("../../testImage_block5.txt");
+
+        }
+
+        [TestMethod]
+        public void TestBlockSizefour() // test block size of 4
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 4);
+            image1.printArray("../../testImage_block4.txt");
+
+        }
+        [TestMethod]
+        public void TestBlockSizesix() // test block size of 4
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 6);
+            image1.printArray("../../testImage_block6.txt");
+
+        }
+
+        [TestMethod]
+        public void TestCreateImageOfBlockFive()
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 5);
+            Bitmap b = image1.toImage();
+            b.Save("../../testImage_block5.bmp");
+        }
+
+        [TestMethod]
+        public void TestCreateImageOfBlockOne()
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 1);
+            Bitmap test_image = image1.toImage();
+            test_image.Save("../../testImage_block1.bmp");
+        }
+        [TestMethod]
+        public void CompareFileTestBlockSizeone() // test block size of 4
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 5);
+            image1.printArray("../../testImagecomp_block5.txt");
+            int filebyte1;
+            int filebyte2;
+            // fs1 contains the newly created array file while fs2 contains the standard to check against for this block size 5
+
+            FileStream fs1 = new FileStream("../../testImagecomp_block5.txt", FileMode.Open);
+            FileStream fs2 = new FileStream("../../created.txt", FileMode.Open);
+            if (fs1.Length != fs2.Length)
+            {
+                fs1.Close();
+                fs2.Close();
+                Assert.IsTrue(false, "Incorrect file created - File lengths are different!");
+                return;
+            }
+
+            do
+            {
+                filebyte1 = fs1.ReadByte();
+                filebyte2 = fs2.ReadByte();
+            } while ((filebyte1 == filebyte2) && (filebyte1 != -1));
+
+            Assert.IsTrue((filebyte1 == filebyte2), "Newly created file and Original file does not match");
+
+            fs1.Close();
+            fs2.Close();
+        }
+
+        [TestMethod]
+        public void TestConnectBlockFive()
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"));
+
+            int width = image1.getXTileNum();
+            int height = image1.getYTileNum();
+            System.Diagnostics.Debug.WriteLine("Height " + height + " Width " + width);
+            FloorTile tile = image1.getTile(2, 4);
+            FloorTile n1, n2, n3, n4;
+            System.Diagnostics.Debug.WriteLine("");
+            List<FloorTile> neighbour = tile.getNeighbours();
+            if (neighbour.Count > 3)
+            {
+                n1 = neighbour[0];
+                n2 = neighbour[1];
+                n3 = neighbour[2];
+                n4 = neighbour[3];
+                System.Diagnostics.Debug.WriteLine("1st \t" + n1.Position.X + "\t" + n1.Position.Y + "\t" + n1.Iswalkable());
+                System.Diagnostics.Debug.WriteLine("2nd \t" + n2.Position.X + "\t" + n2.Position.Y + "\t" + n2.Iswalkable());
+                System.Diagnostics.Debug.WriteLine("3rd \t" + n3.Position.X + "\t" + n3.Position.Y + "\t" + n3.Iswalkable());
+                System.Diagnostics.Debug.WriteLine("4th \t" + n4.Position.X + "\t" + n4.Position.Y + "\t" + n4.Iswalkable());
+
+            }
+            else if (neighbour.Count == 3)
+            {
+                n1 = neighbour[0];
+                n2 = neighbour[1];
+                n3 = neighbour[2];
+                System.Diagnostics.Debug.WriteLine("1st \t" + n1.Position.X + "\t" + n1.Position.Y + "\t" + n1.Iswalkable());
+                System.Diagnostics.Debug.WriteLine("2nd \t" + n2.Position.X + "\t" + n2.Position.Y + "\t" + n2.Iswalkable());
+                System.Diagnostics.Debug.WriteLine("3rd \t" + n3.Position.X + "\t" + n3.Position.Y + "\t" + n3.Iswalkable());
+
+            }
+            else if (neighbour.Count == 2)
+            {
+                n1 = neighbour[0];
+                n2 = neighbour[1];
+                System.Diagnostics.Debug.WriteLine("1st \t" + n1.Position.X + "\t" + n1.Position.Y + "\t" + n1.Iswalkable());
+                System.Diagnostics.Debug.WriteLine("2nd \t" + n2.Position.X + "\t" + n2.Position.Y + "\t" + n2.Iswalkable());
+
+            }
+            else if (neighbour.Count == 1)
+            {
+                n1 = neighbour[0];
+                System.Diagnostics.Debug.WriteLine("1st \t" + n1.Position.X + "\t" + n1.Position.Y + "\t" + n1.Iswalkable());
+
+            }
+            else
+                System.Diagnostics.Debug.WriteLine("No Walkable Neighbours");
+
+
+        }
+
+        [TestMethod]
+        public void TestGetTile() // test block size of 1
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 5);
+            FloorTile tile = image1.getTile(10, 12);
+            System.Diagnostics.Debug.WriteLine(tile.Position.X);
+            System.Diagnostics.Debug.WriteLine(tile.Position.Y);
+            System.Diagnostics.Debug.WriteLine(tile.Iswalkable());
+            Assert.IsTrue((tile.ToString().Equals("1")), "Incorret Tile Returned!");
+
+        }
+
+        [TestMethod]
+        public void TestStartTileUnwalkable() // test block size of 5
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 5);
+            bool result = image1.setStartTile(3, 6);
+            FloorTile tile = image1.getTile(3, 6);
+            bool t_res = tile.IsStart();
+            bool t_res2 = tile.IsTarget();
+            Assert.AreEqual(false, result);
+            System.Diagnostics.Debug.WriteLine("Start:" + t_res);
+            System.Diagnostics.Debug.WriteLine("Target:" + t_res2);
+
+        }
+        [TestMethod]
+        public void TestStartTilewalkable() // test block size of 5
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 5);
+            bool result = image1.setStartTile(3, 4);
+            FloorTile tile = image1.getTile(3, 4);
+            bool t_res = tile.IsStart();
+            bool t_res2 = tile.IsTarget();
+            Assert.AreEqual(true, result);
+            System.Diagnostics.Debug.WriteLine("Start:" + t_res);
+            System.Diagnostics.Debug.WriteLine("Target:" + t_res2);
+
+        }
+        [TestMethod]
+        public void TestTargetTileUnwalkable() // test block size of 5
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 5);
+            bool result = image1.setTargetTile(7, 6);
+            FloorTile tile = image1.getTile(7, 6);
+            bool t_res = tile.IsStart();
+            bool t_res2 = tile.IsTarget();
+            Assert.AreEqual(false, result);
+            System.Diagnostics.Debug.WriteLine("Start:" + t_res);
+            System.Diagnostics.Debug.WriteLine("Target:" + t_res2);
+
+
+        }
+
+        [TestMethod]
+        public void TestTargetTileWalkable() // test block size of 5
+        {
+            FloorPlan image1 = new FloorPlan(new Bitmap("../../testImage.jpg"), 5);
+            bool result = image1.setTargetTile(4, 4);
+            FloorTile tile = image1.getTile(4, 4);
+            bool t_res = tile.IsStart();
+            bool t_res2 = tile.IsTarget();
+            Assert.AreEqual(true, result);
+            System.Diagnostics.Debug.WriteLine("Start:" + t_res);
+            System.Diagnostics.Debug.WriteLine("Target:" + t_res2);
+
+        }
+        /// <summary>
+        ///A test for Connect
+        ///</summary>
+        [TestMethod()]
+        public void ConnectTest()
+        {
+            Image myimage = new Bitmap("../../testImage.jpg"); // TODO: Initialize to an appropriate value
+            FloorPlan target = new FloorPlan(myimage); // TODO: Initialize to an appropriate value
+           
+            target.Connect();
+            Assert.AreEqual(0, 0);
+
+        }
+
+    }
+}
---
+++ SourceCode/FloorPlanAndTile/README.txt
@@ -0,0 +1,17 @@
+Author: Debarati Basu
+Date: 2/10/2010
+----------------------------------------------------
+FloorPlanAndTile Folder contains three sub folders:
+----------------------------------------------------
+1. FloorPlanAndTile:
+--------------------
+Contains files FloorPlan.cs and FloorTile.cs. This two are the base files for FloorPlanProcessing module
+
+2.TestFloorPlanAndTile:
+-------------------------
+Contains FloorPlanTest.cs - with unit test methods for FloorPlan.cs
+
+3.TestResults:
+--------------
+Contains files necessary for running the unit tests. DO NOT DELETE ANY FILES FROM THIS FOLDER.
+
